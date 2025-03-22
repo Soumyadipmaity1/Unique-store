2ee36bf34f1ceb6839d7bad768833c334f1bbb41
Soumyadip Maity <22053029@kiit.ac.in>
2025-03-22 10:15:27 +0530

    add signin entry point to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         contact: resolve(__dirname, "contact.html"),
         products: resolve(__dirname, "products.html"),
         addToCart: resolve(__dirname, "addToCart.html"),
+        signin: resolve(__dirname, "signin.html"),
         // Add more entry points for other HTML files as needed
       },
     },
